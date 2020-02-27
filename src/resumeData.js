3f23f9980c457b4e4c18305f3ebd9871c2e7830c
David Krystall <david.krystall@ontometrics.com>
2020-02-27 00:22:39 -0800

    adapting template, todo: add modal for portfolio previews

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,123 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
+  "imagebaseurl":"https://github.com/dkrystall/",
+  "name": "David Krystall",
+  "role": "Full Stack and Mobile App Developer",
+  "linkedinId":"david-krystall-64646a15",
+  "skypeid": "",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, or play games in my free time.",
+  "socialLinks":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/david-krystall-64646a15/",
+        "className":"fa fa-linkedin"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "name":"github",
+        "url":"https://github.com/dkrystall/",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://twitter.com/retebitall",
+        "className":"fa fa-twitter"
       }
     ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  "aboutme":"I am currently a Software Engineer working in Los Angeles, California. The majority of my time is spent coding iOS applications using Swift with XCode. I've recently begun transitioning into doing front end work with SwiftUI. In my spare code time I also enjoy learning new methodologies and currently studying Functional Programming with Scala. I'm also an avid gamer and movie lover.",
+  "address":"Los Angeles, California, USA",
+  "website":"https://github.com/dkrystall/",
+  "education":[
+    {
+      "UniversityName":"California State University, Los Angeles",
+      "specialization":"Computer Science",
+      "MonthOfPassing":"Dec",
+      "YearOfPassing":"2019",
+      "Achievements":"Computer Vision Leader for Robosub 2019 Team"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Ontometrics",
+      "specialization":"Software Engineer",
+      "MonthOfLeaving":"Feb",
+      "YearOfLeaving":"2020",
+      "Achievements":""
+    },
+    {
+      "CompanyName":"Torres Martinez Tribal TANF",
+      "specialization":"Case Assistant",
+      "MonthOfLeaving":"Feb",
+      "YearOfLeaving":"2015",
+      "Achievements":""
+    }
+  ],
+  "skillsDescription":"I have a wide range of Software Engineering skills. I've developed simple SPA websites using Static Generators and Javascript Frameworks, I have also worked on more complex Web Applications with web servers as well as serverless architectures using AWS. I have worked on Mobile applications, Apple Apps using Swift with Xcode and SwiftUI/UIKit; as well as Android Apps using Kotlin with Jetpack and Jetpack Compose. I've also developed state machines for robotics projects; as well as computer vision systems using Darknet's YOLOv3 nerual network for real-time interaction with autonomous systems. I also have Development Operations experience, deploying TDD solutions using Continuous Integration and Continuous Deployment using JetBrains and Jenkins. I'm also familiar with many Amazon Web Services, I've used Route53, MobileHub, Elastic Beanstalk, S3, Kinesis, AppSync, DynamoDB, RDS, EC2, and AWS Lambda. ",
+  "skills":[
+    {
+      "skillname":"Swift"
+    },
+    {
+      "skillname":"Xcode"
+    },
+    {
+      "skillname":"Kotlin"
+    },
+    {
+      "skillname":"Android"
+    },
+    {
+      "skillname":"Java"
+    },
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS3"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"AWS"
+    },
+    {
+      "skillname":"DevOps"
+    },
+    {
+      "skillname":"Python"
+    },
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/coffee.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/coffee.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",  
+      "imgurl":"images/portfolio/girl.jpg"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/farmerboy.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
